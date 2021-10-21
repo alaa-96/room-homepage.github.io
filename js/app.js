cc8e80d050954bbc3c6628886251a5a7f1a7e917
Alaa Atef <alaa00x0@gmail.com>
2021-10-21 17:51:29 +0200

    initial commit

---
+++ js/app.js
@@ -0,0 +1,70 @@
+const hamburger = document.querySelector('.hamburger');
+const header = document.querySelector('.header');
+const close = document.querySelector('.close');
+const mySlides = document.querySelectorAll('.mySlides');
+const slide = document.getElementById('slide');
+const firstSlide = document.getElementById('first-slide');
+const secondSlide = document.getElementById('second-slide');
+const thirdSlide = document.getElementById('third-slide');
+const leftAngle = document.querySelector('.left');
+const rightAngle = document.querySelector('.right');
+
+var slidesArray = [firstSlide, secondSlide, thirdSlide];
+
+leftAngle.addEventListener('click', prev);
+rightAngle.addEventListener('click', next);
+
+var i = 0;
+
+function next() {
+  if (i < mySlides.length - 1) {
+    i++;
+  }
+  return showSlide();
+}
+
+function prev() {
+  if (i >= mySlides.length - 2) {
+    i--;
+  }
+  return showSlide();
+}
+
+function showSlide() {
+  if (i === 0) {
+    mySlides[0].style.display = 'flex';
+    mySlides[1].style.display = 'none';
+    mySlides[2].style.display = 'none';
+  }
+  if (i === 1) {
+    mySlides[0].style.display = 'none';
+    mySlides[1].style.display = 'flex';
+    mySlides[2].style.display = 'none';
+  }
+  if (i === 2) {
+    mySlides[0].style.display = 'none';
+    mySlides[1].style.display = 'none';
+    mySlides[2].style.display = 'flex';
+  }
+}
+
+hamburger.addEventListener('click', addActiveClass);
+
+function addActiveClass() {
+  if (header.classList.contains('not-active')) {
+    header.classList.remove('not-active');
+    header.classList.add('active');
+    // document.body.style.opacity = '0.5';
+    // document.body.style
+    // document.body.style.background = 'hsl(0, 0%, 63%)';
+  }
+}
+
+close.addEventListener('click', removeActiveClass);
+
+function removeActiveClass() {
+  if (header.classList.contains('active')) {
+    header.classList.remove('active');
+    header.classList.add('not-active');
+  }
+}
